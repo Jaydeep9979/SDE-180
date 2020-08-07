00e7f219a83b8a8342ccf09df868eb9fb0f40ebb
Jaydeep Mahajan <jaydeep9979@gmail.com>
2020-08-07 14:20:58 +0530

    Update 5_maximum-subarray.py

--- 5_maximum-subarray.py
+++ 5_maximum-subarray.py
@@ -6,6 +6,14 @@ class Solution:
             curr_sum=max(ele,ele+curr_sum)
             max_sum=max(max_sum,curr_sum)
         return max_sum
+# another way
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         for i in range(1, len(nums)):
+#             if nums[i - 1] > 0: 
+#                 nums[i] += nums[i - 1]
+#         return max(nums)
+        
         
 # if no element is allowed
 # class Solution:
